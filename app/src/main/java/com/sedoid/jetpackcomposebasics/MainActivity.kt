6784279cb4a9494fa22a77603c43c0bc3c024e55
sedoid <dzekarlson@gmail.com>
2023-09-21 10:33:40 +0100

    initial commit

---
+++ app/src/main/java/com/sedoid/jetpackcomposebasics/MainActivity.kt
@@ -0,0 +1,227 @@
+package com.sedoid.jetpackcomposebasics
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sedoid.jetpackcomposebasics.ui.theme.JetPackComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackComposeBasicsTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(onContinueCLicked:() ->Unit, modifier:Modifier = Modifier){
+
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )    {
+        Text(
+            text = "Getting started with JetpackCompose Basics",
+        )
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueCLicked) {
+            Text("Continue")
+        }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JetPackComposeBasicsTheme {
+        OnboardingScreen(onContinueCLicked = {})
+    }
+}
+
+
+@Composable
+private fun MyApp(modifier:Modifier = Modifier){
+
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
+
+        if(shouldShowOnboarding){
+            OnboardingScreen(onContinueCLicked = { shouldShowOnboarding = false })
+        }else{
+            Greetings()
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+fun MyAppPreview(){
+    JetPackComposeBasicsTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+@Composable
+fun Greetings(modifier:Modifier = Modifier, names: List<String> = List(1000) { "$it" } ){
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(items = names){ name ->
+                Greeting(name = name)
+            }
+    }
+}
+
+@Preview(showBackground = true,widthDp = 320,heightDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "Dark")
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun GreetingsPreview(){
+    JetPackComposeBasicsTheme {
+        Greetings(modifier = Modifier.fillMaxSize())
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(if(expanded.value) 75.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ))
+
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)) {
+
+        Row(modifier = Modifier.padding(25.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello",)
+                Text(
+                    text = "$name",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                    )
+                Text(
+                    text=""
+                )
+            }
+            ElevatedButton(onClick = { expanded.value =  !expanded.value }) {
+                Text(text = if(expanded.value) "Show Less" else "Show more",)
+            }
+
+        }
+
+    }
+
+
+}
+
+@Composable
+private fun CardContent(name:String){
+    var expanded by remember  { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+                )
+            if(expanded){
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+
+                )
+            }
+        }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if(expanded){
+                    stringResource(id = R.string.show_less)
+                }else
+                    stringResource(id = R.string.show_more)
+            )
+        }
+
+    }
+}
+
+
+//@Preview(showBackground = true)f
+//@Composable
+//fun GreetingPreview() {
+//    JetPackComposeBasicsTheme {
+//        MyApp(modifier = Modifier.fillMaxSize(), names = listOf<String>("Karlson","sedoid"))
+//    }
+//}
